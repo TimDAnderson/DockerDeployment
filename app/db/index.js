c7df932858fbe61cbf91e985fd313bdf50412222
Tim Anderson <tdanderson805@gmail.com>
2021-03-29 21:47:20 -0700

    using process.env variables

--- app/db/index.js
+++ app/db/index.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'sqlcontainer',
   user: 'root',
-  password: 'root',
-  database: 'app_db'
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
 
 connection.connect();
